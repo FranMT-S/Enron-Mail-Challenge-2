6ceabc8446263383669f6ae7f646829915f112ae
FranMT-S <franciscomadridtoledo@gmail.com>
2025-02-04 15:25:56 -0600

    start frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import MailsView from '@/modules/mails/MailsView.vue'
+
+</script>
+
+<template>
+    <MailsView/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode:false,
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+}
+
