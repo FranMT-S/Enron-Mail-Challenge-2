e165fbb852af60ccd006b5580c42441cf4710dfc
FranMT-S <franciscomadridtoledo@gmail.com>
2025-02-24 16:31:15 -0600

    add styles css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode:false,
@@ -7,9 +8,31 @@ export default {
     screens:{
       'mobile':"460px"
     },
+    animationDuration:{
+      '500':'500ms',
+      '800':'800ms',
+      '1000':'1000ms',
+      '2000':'2000ms',
+      '3000':'3000ms',
+      '4000':'4000ms',
+      '5000':'5000ms',
+    }
+    ,
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'animation-duration': (value) => ({
+            'animation-duration': value
+          }),
+        },
+        { values: theme('animationDuration') }
+      )
+    })
+
+  ],
 
 }
 
