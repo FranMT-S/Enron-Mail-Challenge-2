b8aee9e373247011bced1729bc159aaa74729717
FranMT-S <franciscomadridtoledo@gmail.com>
2025-02-26 21:22:39 -0600

    change router

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -4,22 +4,22 @@ import MailsView from '@/modules/mails/views/MailsView.vue'
 import MailDetailsView from '@/modules/mails/views/MailsDetailsView.vue'
 import { NamesRouter } from './routesNames'
 const routes = [
-  { path: '/',
-    component: MailsView,
-    name:NamesRouter.Index,
-    // children:[
-    //   {
-    //   path: '/mails',
-    //   component: MailDetailsView,
-    //   name:NamesRouter.MailDetail
-    //   }
-    // ]
-  },
   {
-    path: '/:id',
-    component: MailDetailsView,
-    name:NamesRouter.MailDetail
-  }
+    path: '/',
+    children:[
+      {
+        path: '/',
+        component: MailsView,
+        name:NamesRouter.Index,
+      },
+      {
+        path: '/:id',
+        component: MailDetailsView,
+        name:NamesRouter.MailDetail
+      }
+    ]
+  },
+
 ]
 
 const router = createRouter({
