af6018e999f48c5a27ab762b9104d0c9b6d86296
FranMT-S <franciscomadridtoledo@gmail.com>
2025-02-28 13:40:24 -0600

    fix error component image load

--- frontend/src/modules/mails/components/errors/ErrorComponent.vue
+++ frontend/src/modules/mails/components/errors/ErrorComponent.vue
@@ -11,15 +11,14 @@ interface Props{
   error:Error
 }
 
-const icons = {
+const iconsSvg = {
   wifiError: WifiErrorIcon,
   alert: AlertTriangleIcon,
   wifiNoConnectIcon: WifiNoConnectIcon,
-  search:MagnifyingGlassImage
 };
 
 const props = defineProps<Props>();
-const Image = shallowRef(icons.wifiError)
+const Image = shallowRef(iconsSvg.wifiError)
 const message = ref("")
 const code = ref(500)
 
@@ -29,21 +28,17 @@ const validateError = (error:Error) =>{
     code.value = error.status
     switch (error.status) {
       case 503:
-        Image.value = icons.wifiError
+        Image.value = iconsSvg.wifiError
         break;
       case 408:
-        Image.value = icons.wifiError
+        Image.value = iconsSvg.wifiError
         break;
-      case 404:
-        Image.value = icons.search
-        break;
-
       default:
-        Image.value = icons.alert
+        Image.value = iconsSvg.alert
         break;
     }
   }else{
-    Image.value = icons.alert
+    Image.value = iconsSvg.alert
     code.value = 500
     message.value = "There was a problem try again later"
   }
@@ -57,7 +52,9 @@ onMounted(() => validateError(props.error))
       <div
         :class="{'max-w-[300px]':code == 404}"
         class="flex flex-col max-w-[150px]">
-        <Image class="ico w-full text-[#4f108e]" :class="[':text-[#c338c3]']"/>
+
+        <MagnifyingGlassImage v-if="code == 404" class="ico w-full text-[#4f108e]" :class="[':text-[#c338c3]']"/>
+        <Image v-else class="ico w-full text-[#4f108e]" :class="[':text-[#c338c3]']"/>
       </div>
       <h1 class=" text-[#5000a0] text-lg font-semibold text-center ">{{message}}</h1>
   </div>
