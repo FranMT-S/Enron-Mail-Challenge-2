51b3fd6818a639ae29fddbde91baf3102e93c333
FranMT-S <franciscomadridtoledo@gmail.com>
2025-02-28 23:40:24 -0600

    add gateway terraform

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,8 +7,26 @@ resource "aws_vpc" "vpc_mails" {
   }
 }
 
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.vpc_mails.id
+}
+
+
 resource "aws_subnet" "subnet_mails_client" {
   vpc_id            = aws_vpc.vpc_mails.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = var.availability_zone
+  map_public_ip_on_launch = true
+}
+
+
+resource "aws_route_table" "all_trafic" {
+  vpc_id = aws_vpc.vpc_mails.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+  tags = {
+    Name = "route-table-mails"
+  }
 }
